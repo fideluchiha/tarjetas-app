eeeb2d56c8c96b0803bac9c7ae2a5cdcc123bd8d
fideluchiha <1999fidel@gmail.com>
2023-02-28 03:07:37 -0500

    primer comit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,15 @@
-import { NgModule } from '@angular/core';
+import { Component, NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AgregarComponent } from './Tarjetas/agregar/agregar.component';
+import { ListarComponent } from './Tarjetas/listar/listar.component';
+import { CrearComponent } from './Transacciones/crear/crear.component';
+import { ListarTransaccionComponent } from './Transacciones/listar-transaccion/listar-transaccion.component';
 
-const routes: Routes = [];
+
+const routes: Routes = [{path:'listarTarjetas',component:ListarComponent},
+{path:'agregarTarjetas',component:AgregarComponent},
+{path:'agregarTransaccion',component:CrearComponent},
+{path:'listarTransaccion',component:ListarTransaccionComponent}];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,31 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ListarComponent } from './Tarjetas/listar/listar.component';
+import { AgregarComponent } from './Tarjetas/agregar/agregar.component';
+import { FormsModule }from '@angular/forms';
+import { ServiceService } from 'src/app/Service/service.service';
+import { HttpClientModule } from '@angular/common/http';
+import { ReactiveFormsModule } from '@angular/forms';
+import { CrearComponent } from './Transacciones/crear/crear.component';
+import { ListarTransaccionComponent } from './Transacciones/listar-transaccion/listar-transaccion.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ListarComponent,
+    AgregarComponent,
+    CrearComponent,
+    ListarTransaccionComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule,
+    ReactiveFormsModule,
+    HttpClientModule
   ],
-  providers: [],
+  providers: [ServiceService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
